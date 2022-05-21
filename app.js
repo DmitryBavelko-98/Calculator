c4ccbf4c7cd4178d5421ddf958f196a142fc6dab
DmitryBavelko-98 <bavelko90@gmail.com>
2022-05-21 13:26:10 +0300

    Add basic math operations

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    return operator(a, b);
+}
+
